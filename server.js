528e35e80972d76405aa8efb556b50d64f7dde7d
Jason Ross <jasonorlandoross@gmail.com>
2022-10-04 22:01:48 -0500

    pass it around complete

---
+++ server.js
@@ -0,0 +1,34 @@
+// TAKE ONE DOWN PASS IT AROUND
+const express = require ("express")
+
+const app = express ();
+
+app.get ("/" , (req,res) => {
+    res.send ("<h1> 99 bottles of beer on the wall  <h1> <br>  <a href = 'http://localhost:3000/98 ' > take one down, pass it around </a>  ")
+})
+
+
+app.get ("/:number_of_bottles" , (req,res) => {
+    if (req.params.number_of_bottles === "0" ) {
+        console.log ("test")
+        res.send (`<h1> ${req.params.number_of_bottles} bottles left <h1> <br> <a href = 'http://localhost:3000' > play again </a>  ` )
+    
+      
+    } else 
+    res.send (`<h1> ${req.params.number_of_bottles} bottles left <h1> <br> <a href = 'http://localhost:3000/${req.params.number_of_bottles-1}' > take one down, pass it around </a>  ` )
+})
+
+const multipy = (num1 , num2) => {
+    return num1*num2; 
+}
+
+
+
+
+console.log (multipy (2,4));
+
+app.listen (3000 , ()  => {
+    console.log ("listening in port 3000")
+})
+
+module.exports = multipy;
